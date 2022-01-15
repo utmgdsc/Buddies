2e32202e2d4f2434cf1852cd1ebdc8898296e625
mio <knightopress@gmail.com>
2022-01-15 15:50:10 -0500

    Add Material UI.

--- Buddies.Web/src/pages/_app.tsx
+++ Buddies.Web/src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useMemo } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(() => createTheme({
+    palette: {
+      mode: prefersDarkMode ? 'dark' : 'light',
+    },
+  }), [prefersDarkMode]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
 
 export default MyApp;
