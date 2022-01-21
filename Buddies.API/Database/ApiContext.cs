e0c26bc7cb2cfcdc5d40ad15ec4f0adabd92b439
mio <knightopress@gmail.com>
2022-01-21 18:41:54 -0500

    Enable API connection to PostgreSQL.

---
+++ Buddies.API/Database/ApiContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Buddies.API.Database;
+
+public class ApiContext : DbContext
+{
+    public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+    {
+    }
+}
--- Buddies.API/Program.cs
+++ Buddies.API/Program.cs
@@ -1,3 +1,6 @@
+using Buddies.API.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +13,11 @@ builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true
 // URL versioning
 builder.Services.AddApiVersioning();
 
+// Postgres DB connection
+
+var connectionString = builder.Configuration.GetConnectionString("ApiContext");
+
+builder.Services.AddDbContext<ApiContext>(options => options.UseNpgsql(connectionString));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
