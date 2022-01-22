8fd4362d2f5148889dc259d1e8f2e3905623e463
mio <knightopress@gmail.com>
2022-01-22 18:29:22 -0500

    Add registration form UI.

---
+++ Buddies.Web/src/components/RegisterForm.tsx
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+
+const RegisterForm: React.VFC = () => {
+  return (
+    <Box component="form" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Typography variant="h3" sx={{ marginBottom: 6 }}>Project Buddies</Typography>
+      <TextField label="First Name" fullWidth sx={{ marginBottom: 2 }} />
+      <TextField label="Last Name" fullWidth sx={{ marginBottom: 2 }} />
+      <TextField label="Email" fullWidth sx={{ marginBottom: 2 }} />
+      <TextField label="Password" type="password" fullWidth />
+      <Button type="submit" variant="contained" sx={{ marginTop: 4 }}>Register</Button>
+    </Box>
+  );
+};
+
+export default RegisterForm;
---
+++ Buddies.Web/src/pages/register.tsx
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import Box from '@mui/material/Box';
+import RegisterForm from '../components/RegisterForm';
+
+const Register: React.VFC = () => {
+  return (
+    <Box sx={{
+      width: 0.25, flexGrow: 1, alignSelf: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center',
+    }}
+    >
+      <RegisterForm />
+    </Box>
+  );
+};
+
+export default Register;
