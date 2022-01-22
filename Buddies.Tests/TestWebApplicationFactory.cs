cff8428915310e225ee98d1b5c42198627fe569f
mio <knightopress@gmail.com>
2022-01-22 14:52:10 -0500

    Add registration endpoint tests.

---
+++ Buddies.Tests/TestWebApplicationFactory.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Buddies.API.Database;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Buddies.Tests;
+
+public class TestWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(d => 
+                d.ServiceType == typeof(DbContextOptions<ApiContext>));
+
+            if (descriptor != null) services.Remove(descriptor);
+
+            services.AddDbContext<ApiContext>(options => 
+                options.UseInMemoryDatabase("ApiContext"));
+
+            var provider = services.BuildServiceProvider();
+
+            using (var scope = provider.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<ApiContext>();
+                db.Database.EnsureCreated();
+            }
+            
+        });
+    }
+}
---
+++ Buddies.Tests/User/RegisterTests.cs
@@ -0,0 +1,108 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using Buddies.API.IO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Buddies.Tests.User;
+
+public class RegisterTests : IClassFixture<TestWebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+    private readonly TestWebApplicationFactory<Program> _factory;
+    
+    public RegisterTests(TestWebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+        _client = _factory.CreateClient();
+    }
+    
+    [Fact]
+    public void TestEmptyRequest()
+    {
+        var request = new RegisterRequest();
+        var response = _client.PostAsJsonAsync("/api/v1/users/register", request).Result;
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        var responseBody = response.Content.ReadFromJsonAsync<JsonDocument>().Result;
+        Assert.Equal(4, responseBody?.RootElement.GetProperty("errors").EnumerateObject().Count());
+    }
+
+    [Fact]
+    public void TestInvalidEmail()
+    {
+        var request = new RegisterRequest
+        {
+            Email = "not an email",
+            FirstName = "Akari",
+            LastName = "Akaza",
+            Password = "Pwd^123"
+        };
+        
+        var response = _client.PostAsJsonAsync("/api/v1/users/register", request).Result;
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        var responseBody = response.Content.ReadFromJsonAsync<JsonDocument>().Result;
+        Assert.True(responseBody?.RootElement.GetProperty("errors").TryGetProperty("Email", out _));
+    }
+
+    [Fact]
+    public void TestUnsafePassword()
+    {
+        var request = new RegisterRequest
+        {
+            Email = "akari@akaza.moe",
+            FirstName = "Akari",
+            LastName = "Akaza",
+            Password = "12345"
+        };
+
+        var response = _client.PostAsJsonAsync("/api/v1/users/register", request).Result;
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        var responseBody = response.Content.ReadFromJsonAsync<JsonDocument>().Result;
+        Assert.True(responseBody?.RootElement.GetProperty("errors").TryGetProperty("Password", out _));
+    }
+
+    [Fact]
+    public void TestShortName()
+    {
+        var request = new RegisterRequest
+        {
+            Email = "akari@akaza.moe",
+            FirstName = "A",
+            LastName = "A",
+            Password = "Pwd^123"
+        };
+        
+        var response = _client.PostAsJsonAsync("/api/v1/users/register", request).Result;
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        var responseBody = response.Content.ReadFromJsonAsync<JsonDocument>().Result;
+        Assert.True(responseBody?.RootElement.GetProperty("errors").TryGetProperty("FirstName", out _));
+        Assert.True(responseBody?.RootElement.GetProperty("errors").TryGetProperty("LastName", out _));
+    }
+
+    [Fact]
+    public void TestUserRegistered()
+    {
+        var request = new RegisterRequest
+        {
+            Email = "akari@akaza.moe",
+            FirstName = "Akari",
+            LastName = "Akaza",
+            Password = "Pwd^123"
+        };
+        
+        var response = _client.PostAsJsonAsync("/api/v1/users/register", request).Result;
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        var scopeFactory = _factory.Services.GetRequiredService<IServiceScopeFactory>();
+        using (var scope = scopeFactory.CreateScope())
+        {
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<API.Entities.User>>();
+            var user = userManager.Users.FirstOrDefault(user => user.Email == request.Email);
+            Assert.NotNull(user);
+        }
+    }
+}
