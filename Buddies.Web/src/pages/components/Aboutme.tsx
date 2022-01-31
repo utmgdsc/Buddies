e6665e00c9fa8abcecaf1a4b3ff2a4ff6b8f50ba
stalhaali <stalhaali1234@gmail.com>
2022-01-31 05:52:49 -0500

    Added Aboutme component for Profile Page

---
+++ Buddies.Web/src/pages/components/Aboutme.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@material-ui/core/grid';
+import Typography from '@mui/material/Typography';
+
+ 
+const Aboutme = ({desc}) => {  
+    return (
+        <Box p = {2} boxShadow={12} sx={{ width: '100%', height: 100, backgroundColor: 'white', border: 1, alignItems: "center"}}>
+            <Grid container > 
+                <Grid item>
+                    <Typography variant="h6" gutterBottom>
+                        About me
+                    </Typography> 
+                    <Typography variant="subtitle2" gutterBottom>
+                        {desc}
+                    </Typography> 
+                </Grid>
+            </Grid>
+        </Box>     
+    )
+}
+export default Aboutme;
+
