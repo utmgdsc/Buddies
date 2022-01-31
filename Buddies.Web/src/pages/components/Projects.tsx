85a0fd2ecfbff10f48fbd36e1d69c006e1713672
stalhaali <stalhaali1234@gmail.com>
2022-01-31 05:54:36 -0500

    Added Projects Component for Profile Page

---
+++ Buddies.Web/src/pages/components/Projects.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@material-ui/core/grid';
+import Typography from '@mui/material/Typography';
+import Button from '@material-ui/core/Button'
+
+ 
+const Projects = () => {  
+    return (
+        <Box p = {2} boxShadow={12} sx={{ width: '100%', height: 300, maxHeight:"300px", backgroundColor: 'white', border: 1, alignItems: "center", overflowY:'auto'}}>
+            <Grid container > 
+                <Grid item>
+                    <Typography variant="h6" gutterBottom>
+                        Projects
+                    </Typography> 
+                    <Typography variant="subtitle2" gutterBottom>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                    </Typography> 
+                </Grid>
+            </Grid>
+        </Box>     
+    )
+}
+export default Projects;
+
