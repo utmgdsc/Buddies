4c959257f7e41c584bf759afd89448aa8b217c1a
stalhaali <stalhaali1234@gmail.com>
2022-02-04 07:18:39 -0500

    Updated to Axios

--- Buddies.Web/src/pages/components/Profile.tsx
+++ Buddies.Web/src/pages/components/Profile.tsx
@@ -8,89 +8,108 @@ import Websites from './Websites';
 import { Container } from '@material-ui/core';
 import Box from '@mui/material/Box';
 import Grid from '@material-ui/core/grid';
-import { makeStyles } from '@material-ui/core'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-{/* Profile page. I intend to add api end points, and using those I want
-  to get the specific User that's logged in. Then I can pass in his information
-  into each component as a prop. Currently, I'm just using default values. 
+const api = axios.create({
+  baseURL: "https://localhost:7128/api/v1/Profiles/1",
+  headers: {
+    'Accept': 'application/json',
+    'Content-type': 'application/json'
+  }
+})
+
+type Skillobject = {
+  "id": number,
+  "name": string,
+  "delete": boolean
+}
+
+type UpdateProf = {
+  "id": number,
+  "name": string,
+  "bio": string,
+  "aboutme": string,
+  "skills": Skillobject[]
+}
+
+
+{/* Profile page. Responsible for putting all the components that make up the profile
+  page together. It also sends GET requests to get a user's profile by his id. And a
+  PUT request, to update a user's profile, if he makes any changes. 
 */}
 
-const desc: string = "Hello my Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.dsaadsdjddsklfjklfdaskjlfdasfddfkladsfkjfsdkjldfsklajfdslkfdjskldskdfljdfskjlfdjfdfdffdfdfffffff";
 const Profile = () =>  {
-  const [profile, setprofile] = useState([]);
 
-  {/* Get and Put Requests. I will later modify this to use a specific id,
+  const [userProfile, setProfile] = useState([{"id": 0, "name": "joe", "bio": "n/a", "aboutme": "n/a", "skills": [{"id": 1, "name": "Data Structures", "delete": false}, {"id": 2, "name": "C++", "delete": false}, {"id": 3, "name": "Python", "delete": false}]}]);
+
+  let loggedin: boolean = true; {/* Since the log in feature has not been created yet, I use a boolean to toggle
+     on and off the functionality for a user thats logged in and logged out. (The logged in functionality only
+      allows for edit access)
+     */}
+
+  useEffect(() => { {/* When the page loads, a get request is made to populate the profile page accordingly */}
+    getProfile();
+  }, [])
+
+  const profileToUpdate: UpdateProf = {  
+    "id": userProfile.id,
+    "name": userProfile.name,
+    "bio": userProfile.bio,
+    "aboutme": userProfile.aboutme,
+    "skills": userProfile.skills
+  }
+    {/* Get and Put Requests. I will later modify this to use a specific id,
     to get the id of the user profile being accessed. For testing,
-    I am just using hard coded values. 
-*/}
+    I am just using hard coded values.
+  */}
   function getProfile() {
-    const url: string = "https://localhost:7128/api/v1/Profiles/3"
-
-    fetch(url, {
-      method: 'GET'
-    }).then(response => response.json())
-    .then(profileFomServer => {
-      console.log(profileFomServer);
-      setprofile(profileFomServer);
+    api.get('/').then(res => {
+      console.log(res.data)
+      res.data.skills = profileToUpdate.skills;
+      setProfile(res.data);
+      console.log(userProfile.name);
     }).catch((error) => {
       console.log(error);
       alert(error);
     });
+
+    
   }
 
-  const postToUpdate = {
-    "id": 1,
-    "name": 'Bill',
-    "email": "john@gmail.com",
-    "occup": 'boy'
-  }
-
-  function updateProfile() {
-    const url: string = "https://localhost:7128/api/v1/Profiles/1"
-
-    fetch(url, {
-      method: 'PUT',
-      headers: {
-        'Accept': 'application/json',
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(postToUpdate)
-      
-    }).then(response => response.json())
-    .then(profileFomServer => {
-      console.log(profileFomServer);
-      
-    }).catch((error) => {
+  let updateProfile: VoidFunction = async () => {
+    let res = await api.put(`/`, profileToUpdate).catch((error) => {
       console.log(error);
       alert(error);
     });
+    console.log(res);
+    console.log(profileToUpdate);
+    res.data.skills = profileToUpdate.skills;
+    console.log(res.data);
+    setProfile(res.data);
+
+    console.log(userProfile);
   }
 
-   
+
   return (
   <>
     <Navbar /> 
-    <button onClick={getProfile}></button> {/* Ignore this. Just for testing */}
-    <button onClick={updateProfile}></button> 
     <Box p = {5} >
       <Container>
         <Grid container spacing={5} alignItems ='center' justifyContent= 'center'>
-          <Header name={profile.name} email={profile.email} occupation={profile.occup}/>
+          <Header updateFunc={updateProfile} newProfile={profileToUpdate} logCheck={loggedin} name={userProfile.name} bio={userProfile.bio}/>
           <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
-            <BScore score={81} />  
+            <BScore score={0} />  
+            <br />
+            <Skills updateFunc={updateProfile} newProfile={profileToUpdate} logCheck={loggedin} />
+            <br />
+            <Websites logCheck={loggedin} />
           </Grid>
           <Grid item xs={12} sm={12} md={9} lg={9} xl={9}>
-            <Aboutme desc={desc}/> 
-          </Grid>
-          <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
-            <Skills />
+            <Aboutme updateFunc={updateProfile} newProfile={profileToUpdate} logCheck={loggedin} desc={userProfile.aboutme}/>
             <br />
-            <Websites />
-          </Grid>
-          <Grid item xs={12} sm={12} md={9} lg={9} xl={9}>
-            <br />
-            <Projects />
+            <Projects /> 
           </Grid>
          
         </Grid> 
@@ -101,3 +120,4 @@ const Profile = () =>  {
 };
 
 export default Profile;
+export type { UpdateProf };
