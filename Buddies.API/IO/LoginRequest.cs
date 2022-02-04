82e70cea17f66569164569227f802b77e75986bf
Tian Yue Dong <tianyue.dong@mail.utoronto.ca>
2022-02-04 03:59:52 -0500

    added jwt dependencies, setup jwt in Program.cs and appsettings, setup access token logic in UserController

---
+++ Buddies.API/IO/LoginRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Buddies.API.IO;
+
+/// <summary>
+/// A registration request.
+/// </summary>
+/// <param name="FirstName">First name of user.</param>
+/// <param name="LastName">Last name of user.</param>
+/// <param name="Email">Email of user.</param>
+/// <param name="Password">Password of user.</param>
+public record LoginRequest(
+    [Required(ErrorMessage = "A email is required.")]
+    string Email,
+    [Required(ErrorMessage = "A password is required.")]
+    string Password
+);
--- Buddies.API/Program.cs
+++ Buddies.API/Program.cs
@@ -1,7 +1,10 @@
 using Buddies.API.Database;
 using Buddies.API.Entities;
 using Buddies.API.IO;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,6 +30,22 @@ builder.Services.AddDbContext<ApiContext>(options => options.UseNpgsql(connectio
 builder.Services.AddIdentity<User, Role>(options => options.User.RequireUniqueEmail = true)
     .AddEntityFrameworkStores<ApiContext>()
     .AddErrorDescriber<CustomIdentityErrorDescriber>();
+
+// set up JWT 
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                    };
+                });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
