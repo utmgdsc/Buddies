04a528e9d794c60ba832bfc8b174d5f0010c38eb
mio <knightopress@gmail.com>
2022-02-06 20:00:24 -0500

    Document AuthState type.

--- Buddies.Web/src/stores/authStore.ts
+++ Buddies.Web/src/stores/authStore.ts
@@ -1,10 +1,19 @@
 ﻿import create from 'zustand';
 import { JwtPayload } from 'jwt-decode';
 
+// look into generating this from the backend in the future
 export interface AuthState extends JwtPayload {
-  // look into generating these from the backend in the future
+  /**
+   * ID of the currently logged in user.
+   */
   nameid: string,
+  /**
+   * First name of the currently logged in user.
+   */
   given_name: string,
+  /**
+   * Last name of the currently logged in user.
+   */
   family_name: string
 }
 
