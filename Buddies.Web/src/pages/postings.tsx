c88bf3b832e3d6264288b3616c2f1ef50355bb02
stalhaali <stalhaali1234@gmail.com>
2022-03-02 06:39:10 -0500

    Create project postings page, Add filter component

---
+++ Buddies.Web/src/pages/postings.tsx
@@ -0,0 +1,192 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Avatar from '@mui/material/Avatar';
+import Grid from '@material-ui/core/grid';
+import Typography from '@mui/material/Typography';
+import { Box, Button, InputLabel } from '@mui/material';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import TablePagination from '@mui/material/TablePagination';
+import TableFooter from '@mui/material/TableFooter';
+import { Container, FormControl } from '@material-ui/core';
+import LegendToggleIcon from '@mui/icons-material/LegendToggle';
+import MultipleSelectPlaceholder from '../components/Filter';
+
+type Project = {
+    Title: string,
+    Description: string,
+    Location: string,
+    Username: string,
+    BuddyScore: number,
+    Members: number
+}
+let PROJECTS: Project[] = []; //fake data
+for (let i=0; i<14; i++){
+    PROJECTS[i] = {
+        Title: 'MP3 to JPG Web App ' + i,
+        Description: 'Description ' + i,
+        Location: 'Mississauga, ON, CAN',
+        Username: 'Jack ' + i,
+        BuddyScore: i,
+        Members: 3
+    }
+}
+
+const memberfilters: string[] = [
+    'Members: 2',
+    'Members: 3',
+    'Members: 4',
+    'Members: 5',
+    'Members: 6',
+    'Members: 7',
+    'Members: 8',
+    'Members: 9',
+    'Members: 10',
+    'Members: 11',
+  ];
+
+  const locations: string[] = [
+    'Location: 2',
+    'Location: 3',
+    'Location: 4',
+    'Location: 5',
+    'Location: 6',
+    'Location: 7',
+    'Location: 8',
+    'Location: 9',
+    'Location: 10',
+    'Location: 11',
+  ];
+
+  const categories: string[] = [
+    'Category: 2',
+    'Category: 3',
+    'Category: 4',
+    'Category: 5',
+    'Category: 6',
+    'Category: 7',
+    'Category: 8',
+    'Category: 9',
+    'Category: 10',
+    'Category: 11',
+  ];
+
+const PostingsTable = () => {
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(4);
+
+    
+    const handleChangePage = (e: unknown, newPage: number) => {
+      setPage(newPage);
+    };
+  
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setRowsPerPage(+event.target.value);
+      setPage(0);
+    };
+
+    return (
+        <Container>
+            <Grid container>
+                <Grid item xs={12} style={{marginTop: 20}}>
+                    <Grid container>
+                        <Grid item xs={12} sm={12} md={8}>
+                            <Typography sx={{marginTop: 4, marginLeft: 1 }} variant="h4">
+                                Recent Postings
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={4}>
+                            <LegendToggleIcon sx={{marginTop: 5}} />
+                            <MultipleSelectPlaceholder placeholder='Category' names={categories}/>
+                            <MultipleSelectPlaceholder placeholder='Members' names={memberfilters} />
+                            <MultipleSelectPlaceholder placeholder='Location' names={locations}/>
+                        </Grid>
+                    </Grid>
+                    <Box sx={{width: '100%', backgroundColor: 'black', height: 10, marginBottom: 5}}/>            
+                    <TableContainer component={Paper} sx={{width: '100%', marginleft: 5}}>
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                            <TableHead>
+                            <TableRow>
+                                <TableCell>Projects</TableCell>
+                                <TableCell>Members</TableCell>
+                            </TableRow>
+                            </TableHead>
+                            <TableBody>
+                            {PROJECTS.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
+                                <TableRow
+                                key={row.Title}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        <Grid container>
+                                            <Grid item>
+                                                <Typography variant="h5">
+                                                    {row.Title}
+                                                </Typography>
+                                                <Grid container>
+                                                    <Avatar />
+                                                    <Typography sx={{marginTop: 2, marginLeft: 1 }} variant="subtitle2">
+                                                        {row.Username}
+
+                                                    </Typography>
+                                                    <Button
+                                                        variant="contained"
+                                                        style={{
+                                                        color: 'white',
+                                                        backgroundColor: 'green',
+                                                        maxWidth: '25px',
+                                                        maxHeight: '25px',
+                                                        minWidth: '25px',
+                                                        minHeight: '25px',
+                                                        marginLeft: 5,
+                                                        marginTop: 10
+                                                        }}
+                                                    >
+                                                        {row.BuddyScore}
+                                                    </Button>
+                                                    <LocationOnIcon sx={{marginTop: 1, marginLeft: 2}} />
+                                                    <Typography sx={{marginTop: 2 }} variant="subtitle2">
+                                                        {row.Location}
+                                                    </Typography>
+                                                </Grid>
+                                            </Grid>
+                                        </Grid>
+                                    </TableCell>
+                                    <TableCell>
+                                        <Grid container>
+                                            <PeopleAltIcon sx={{marginTop: 1, marginRight: 2}} />
+                                            <Typography sx={{marginTop: 1 }} variant="subtitle2">
+                                                        {row.Members}/4
+                                            </Typography>
+                                        </Grid>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                            </TableBody>
+                            <TableFooter>
+                                <TablePagination
+                            rowsPerPageOptions={[5, 10, 15]}
+                            component="div"
+                            count={PROJECTS.length}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            onPageChange={handleChangePage}
+                            onRowsPerPageChange={handleChangeRowsPerPage}
+                            />
+                            </TableFooter>
+                            
+                        </Table>
+                    </TableContainer>
+                </Grid>    
+            </Grid>
+        </Container>
+    )
+};
+
+export default PostingsTable;
