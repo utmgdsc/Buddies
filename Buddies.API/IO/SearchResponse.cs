baf4a078e1213e7afd3804f15c82120e9dd59876
stalhaali <stalhaali1234@gmail.com>
2022-03-03 05:30:11 -0500

    Create paginated endpoints with fuzzysearch

---
+++ Buddies.API/IO/SearchResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Buddies.API.IO
+{
+    public class SearchResponse
+    {
+        public List<string> Searches { get; set; } = new List<string>();
+        public int TotalPages { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
