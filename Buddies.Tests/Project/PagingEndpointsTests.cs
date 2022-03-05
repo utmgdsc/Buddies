f8be4f61649465b36b0b42923b100c0b44904093
SyedTahaA <awesome.taha.47@gmail.com>
2022-03-05 18:37:57 -0500

    Add paging endpoints tests

---
+++ Buddies.Tests/Project/PagingEndpointsTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Buddies.API.IO;
+using Xunit;
+
+namespace Buddies.Tests.Project
+{
+    public class PagingEndpointsTests : IClassFixture<TestWebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+        private readonly TestWebApplicationFactory<Program> _factory;
+
+        public PagingEndpointsTests(TestWebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _client = _factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task SafeBadCategoryRequest()
+        {
+            var response = await _client.GetAsync("/api/v1/category/sdaasdasd/2020/2020");
+            Assert.True(!response.IsSuccessStatusCode);
+
+        }
+
+        [Fact]
+        public async Task SafeBadLocationRequest()
+        {
+            var response = await _client.GetAsync("/api/v1/locations/sdaasdasd/2020/2020");
+            Assert.True(!response.IsSuccessStatusCode);
+
+        }
+
+        [Fact]
+        public async Task SafeBadEmailsRequest()
+        {
+            var response = await _client.GetAsync("/api/v1/email/sdaasdasd/2020/2020");
+            Assert.True(!response.IsSuccessStatusCode);
+
+        }
+
+    }
+}
