4475e18d077dcdfd8104ecb05ced7453396488c5
mio <knightopress@gmail.com>
2022-03-20 18:28:25 -0400

    Run linter.

--- Buddies.Web/src/components/RemoveDialog.tsx
+++ Buddies.Web/src/components/RemoveDialog.tsx
@@ -1,4 +1,4 @@
-﻿import React from 'react';
+import React from 'react';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
