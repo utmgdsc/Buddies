4a999baea82aa12b9e828238937a0dbe69707a4f
SyedTahaA <awesome.taha.47@gmail.com>
2022-03-27 23:26:20 -0400

    Add Notification Response IO

---
+++ Buddies.API/IO/NotificationsResponse.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Buddies.API.IO
+{
+    public class NotificationResponse
+    {
+        [Required]
+        public int Noti_id { get; set; } = default!;
+
+        [Required]
+        public string Message { get; set; } = default!;
+
+        [Required]
+        public int Sender_id { get; set; } = default!;
+
+        [Required]
+        public string Sender_name { get; set; } = default!;
+
+        [Required]
+        public int Project_id { get; set; } = default!;
+
+        [Required]
+        public string Noti_type { get; set; } = default!;
+
+    }
+}
