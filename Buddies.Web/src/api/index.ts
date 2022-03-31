982b3ca899476d80653fe4baeb21971083803b94
mio <knightopress@gmail.com>
2022-03-30 23:20:42 -0400

    Fix import errors.

--- Buddies.Web/src/api/index.ts
+++ Buddies.Web/src/api/index.ts
@@ -9,7 +9,7 @@ import { CreateProjectRequest } from './model/createProjectRequest';
 import { SearchResponse } from './model/searchResponse';
 import { InviteUserRequest } from './model/inviteUserRequest';
 import { RateBuddiesRequest } from './model/rateBuddiesRequest';
-import { UpdateProf } from '../pages/profiles/[pid]';
+import type { UpdateProf } from '../pages/Profiles/[pid]';
 
 export async function registerUser(request: RegisterRequest) {
   return axios.post('/api/v1/users/register', request);
