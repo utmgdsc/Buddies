114a096823d9ed7c270cc4b86ba6856e96081af2
SyedTahaA <awesome.taha.47@gmail.com>
2022-04-22 16:50:55 -0400

    Fix Lint Errors

--- Buddies.Web/src/components/NotificationBell.tsx
+++ Buddies.Web/src/components/NotificationBell.tsx
@@ -1,64 +1,48 @@
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
+const NotificationBell = () => {
+  const [active, setActive] = useState(false);
 
-type notiObject = {
-    'notificationId': number,
-    'message': string,
-    'senderId': number,
-    'senderName': string,
-    'projectId': number,
-    'isRead': boolean,
-    'timeCreated': string,
+  const checkNew = () => {
+    axios
+      .get('/api/v1/notifications/1/1/')
+      .then(({ data } : any) => {
+        if (data.notifications.length !== 0) {
+          if (data.notifications[0].isRead === false) {
+            setActive(true);
+          } else {
+            setActive(false);
+          }
+        } else {
+          setActive(false);
+        }
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+
+  useEffect(() => {
+    checkNew();
+  }, []);
+
+  setInterval(checkNew, 10000);
+
+  return (
+    <>
+      {
+            active === true
+            && <NotificationsActiveIcon />
+        }
+      {
+            active === false
+            && <NotificationsNoneIcon />
+        }
+    </>
+  );
 };
 
-type notiList = {
-    'notifications': notiObject[],
-    'totalPages': number,
-    'currentPage': number,
-}
-
-export default function NotificationBell() {
-    const [active, setActive] = useState(false);
-    
-    const checkNew = () => {
-        const currNoti:notiObject[] = [];
-        axios
-        .get(`/api/v1/notifications/1/1/`)
-        .then(({ data } : any) => {
-            if (data.notifications.length != 0) {
-                if (data.notifications[0].isRead == false) {
-                    setActive(true);
-                } else {
-                    setActive(false);
-                }
-            } else {
-                setActive(false);
-            }
-        })
-        .catch((error) => {
-            alert(error);
-          });
-      };
-
-      useEffect(() => {
-        checkNew();
-      }, []);
-
-      setInterval(checkNew, 10000);
-   
-    return (
-        <>
-        {
-            active == true &&
-            <NotificationsActiveIcon/>
-        } 
-        {
-            active == false &&
-            <NotificationsNoneIcon/>
-        }
-        </>
-    );
-}
+export default NotificationBell;
--- Buddies.Web/src/pages/notifications.tsx
+++ Buddies.Web/src/pages/notifications.tsx
@@ -4,11 +4,10 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import Grid from '@mui/material/Grid';
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import CardContent from '@mui/material/CardContent';
 import CardActionArea from '@mui/material/CardActionArea';
-
 
 // const getapi = axios.create({
 //     baseURL: '/api/v1/notifications/',
@@ -18,119 +17,123 @@ import CardActionArea from '@mui/material/CardActionArea';
 //     },
 // });
 
-type notiObject = {
-    'notificationId': number,
-    'message': string,
-    'senderId': number,
-    'senderName': string,
-    'projectId': number,
-    'isRead': boolean,
-    'timeCreated': string
+type NotiObject = {
+  'notificationId': number,
+  'message': string,
+  'senderId': number,
+  'senderName': string,
+  'projectId': number,
+  'isRead': boolean,
+  'timeCreated': string
 };
 
-type notiList = {
-    'notifications': notiObject[],
-    'totalPages': number,
-    'currentPage': number,
-}
-
-export default function Notifications() {
-    let currentPage = 1;
-    let size = 20;
-    const [notis, setNotis] = useState<notiObject[]>([]); 
-    console.log(notis)
-    const [bot, setBot] = useState<boolean>(false);
-    const loadNotis = () => {
-        const currNoti:notiObject[] = [];
-        axios
-        .get(`/api/v1/notifications/${currentPage}/${size}/`)
-        .then(({ data } : any) => {
-            if (data.totalPages == data.currentPage) {
-                setBot(true);
-            }
-            data.notifications.forEach((p: notiObject) => currNoti.push(p));
-            setNotis((noti) => [...noti, ...currNoti]);
-        })
-        .catch((error) => {
-            alert(error);
-          });
-        currentPage += 1;
-      };
-
-    const handleScroll = (e:any) => {
-        if (bot) {
-            return;
+const Notifications = () => {
+  let currentPage = 1;
+  const size = 20;
+  const [notis, setNotis] = useState<NotiObject[]>([]);
+  console.log(notis);
+  const [bot, setBot] = useState<boolean>(false);
+  const loadNotis = () => {
+    const currNoti:NotiObject[] = [];
+    axios
+      .get(`/api/v1/notifications/${currentPage}/${size}/`)
+      .then(({ data } : any) => {
+        if (data.totalPages === data.currentPage) {
+          setBot(true);
         }
-        if (Math.ceil(e.target.documentElement.scrollTop + window.innerHeight)
+        data.notifications.forEach((p: NotiObject) => currNoti.push(p));
+        setNotis((noti) => [...noti, ...currNoti]);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+    currentPage += 1;
+  };
+
+  const handleScroll = (e:any) => {
+    if (bot) {
+      return;
+    }
+    if (Math.ceil(e.target.documentElement.scrollTop + window.innerHeight)
             >= e.target.documentElement.scrollHeight) {
-            loadNotis();
-        }
-    };
+      loadNotis();
+    }
+  };
 
-    const deleteNoti = (id:number, e:notiObject, index:number) => {
-        axios.put('/api/v1/notifications/read/' + id)
-        .then(() => {
-            let newNotis:notiObject[] = [...notis];
-            e.isRead = true;
-            newNotis[index] = e;
-            setNotis((newNotis))
-        })
-    };
+  const deleteNoti = (id:number, e:NotiObject, index:number) => {
+    axios.put(`/api/v1/notifications/read/${id}`)
+      .then(() => {
+        const newNotis:NotiObject[] = [...notis];
+        e.isRead = true;
+        newNotis[index] = e;
+        setNotis((newNotis));
+      });
+  };
 
-    useEffect(() => {
-        loadNotis();
-        window.addEventListener("scroll", handleScroll);
-      }, []);
+  useEffect(() => {
+    loadNotis();
+    window.addEventListener('scroll', handleScroll);
+  }, []);
 
-    return (
-        <Grid container justifyContent="center" marginTop={3} spacing={3}>
-            <Grid item xs={6}>
-                <Container sx={{ display: 'flex' }}> 
-                    <Typography variant="h3">
-                        Notifications
-                    </Typography>
-                    <NotificationsIcon sx={{marginTop: 1.25, fontSize: 40}}/>
-                </Container>
-                {notis.map((p:notiObject, index) => {
-                    return (
-                    <Card sx={{marginBottom: 2, marginTop: 5, color: p.isRead ? "gray" : "default"}}>
-                        <Container sx={{display: 'flex', marginLeft: 0}}>
-                            <Grid item xs={11}>
-                            <Container sx={{display: 'flex', marginLeft: -2}}>
-                                <a href={`../Profiles/${p.senderId}`}>
-                                <Avatar sx={{marginTop: 2}}/>
-                                </a>
-                                <Container>
-                                    <Typography variant="h6" sx={{marginTop: 1}}>
-                                        {p.senderName}
-                                    </Typography>
-                                    <Typography variant="caption" >
-                                        {p.timeCreated}
-                                    </Typography>
-                                </Container>
-                            </Container>
-                            </Grid>
-                            {
-                            p.isRead == false &&
-                            <Button variant="outlined" color="secondary" onClick={
-                                () => deleteNoti(p.notificationId, p, index)}
-                            >Read</Button>
-                            }
-                        </Container>
-                        <CardContent sx={{marginLeft: 2}}>
-                            <Typography variant="subtitle2" sx={{marginTop: 1}}>
-                                {p.message}
-                            </Typography>
-                            <CardActionArea href={`../projects/${p.projectId}`}>
-                                <Typography variant="subtitle2" >
-                                    Project Link
-                                </Typography>
-                            </CardActionArea>
-                        </CardContent>
-                    </Card>
-                    );
-                })}
-            </Grid>
-        </Grid>
-    );
+  return (
+    <Grid container justifyContent="center" marginTop={3} spacing={3}>
+      <Grid item xs={6}>
+        <Container sx={{ display: 'flex' }}>
+          <Typography variant="h3">
+            Notifications
+          </Typography>
+          <NotificationsIcon sx={{ marginTop: 1.25, fontSize: 40 }} />
+        </Container>
+        {notis.map((p:NotiObject, index) => {
+          return (
+            <Card sx={{ marginBottom: 2, marginTop: 5, color: p.isRead ? 'gray' : 'default' }}>
+              <Container sx={{ display: 'flex', marginLeft: 0 }}>
+                <Grid item xs={11}>
+                  <Container sx={{ display: 'flex', marginLeft: -2 }}>
+                    <a href={`../Profiles/${p.senderId}`}>
+                      <Avatar sx={{ marginTop: 2 }} />
+                    </a>
+                    <Container>
+                      <Typography variant="h6" sx={{ marginTop: 1 }}>
+                        {p.senderName}
+                      </Typography>
+                      <Typography variant="caption">
+                        {p.timeCreated}
+                      </Typography>
+                    </Container>
+                  </Container>
+                </Grid>
+                {
+                            p.isRead === false
+                            && (
+                            <Button
+                              variant="outlined"
+                              color="secondary"
+                              onClick={
+                                () => deleteNoti(p.notificationId, p, index)
 }
+                            >
+                              Read
+                            </Button>
+                            )
+                            }
+              </Container>
+              <CardContent sx={{ marginLeft: 2 }}>
+                <Typography variant="subtitle2" sx={{ marginTop: 1 }}>
+                  {p.message}
+                </Typography>
+                <CardActionArea href={`../projects/${p.projectId}`}>
+                  <Typography variant="subtitle2">
+                    Project Link
+                  </Typography>
+                </CardActionArea>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Notifications;
