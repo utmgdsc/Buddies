c970070a2a4fcd6251c79e3b1b4470615c93c19e
SyedTahaA <awesome.taha.47@gmail.com>
2022-04-24 19:41:53 -0400

    Replace images with free to use images

--- Buddies.Web/src/components/home.tsx
+++ Buddies.Web/src/components/home.tsx
@@ -11,44 +11,56 @@ import Grid from '@mui/material/Grid';
 const cards = [
   {
     id: 1,
-    image: 'https://m.media-amazon.com/images/I/41jLBhDISxL._SY355_.jpg',
+    image: 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg',
     title: 'Customize your Profile!',
     body: 'Let others know about your strengths and weaknesses! Describe what type of projects you like working on.',
+    sourcetext: 'Male face vector created by studiogstock - www.freepik.com',
+    source: 'https://www.freepik.com/vectors/male-face',
   },
 
   {
     id: 2,
-    image: 'https://aem.dropbox.com/cms/content/dam/dropbox/blog/files/2015/10/campus-cup-leaderboard.png',
+    image: 'https://img.freepik.com/free-vector/top-10-best-podium-award_23-2148795639.jpg',
     title: 'Compete to be the best buddy!',
-    body: 'Be the best buddy you can to grow your buddy score. Aim for the top spot on the leaderboard for bragging rights',
+    body: 'Be the best buddy you can to grow your buddy score. Aim for the top spot on the leaderboard for bragging rights.',
+    sourcetext: 'Top 10 vector created by freepik - www.freepik.com',
+    source: 'https://www.freepik.com/vectors/top-10',
   },
 
   {
     id: 3,
-    image: 'https://www.insidehighered.com/sites/default/server_files/media/GettyImages-1208313447.jpg',
+    image: 'https://img.freepik.com/free-vector/business-team-putting-together-jigsaw-puzzle-isolated-flat-vector-illustration-cartoon-partners-working-connection-teamwork-partnership-cooperation-concept_74855-9814.jpg',
     title: 'Create your own projects!',
-    body: 'Create projects with custom requirements and get tailored buddy recommendations that match your project needs!',
+    body: 'Create projects with custom requirements and get tailored buddy recommendations that match your project\'s needs!',
+    sourcetext: 'Teamwork people vector created by pch.vector - www.freepik.com',
+    source: 'https://www.freepik.com/vectors/teamwork-people',
   },
 
   {
     id: 4,
-    image: 'https://crmgamified.com/wp-content/uploads/2020/09/Sales-Management-drawing-01-e1602347137275.png',
-    title: 'Search for Projects',
-    body: 'Search for projects via custom filters like category, location, number of members.',
+    image: 'https://img.freepik.com/free-vector/illustration-magnifying-glass_53876-28516.jpg',
+    title: 'Search for Projects!',
+    body: 'Search for projects via custom filters like category, location, and number of members.',
+    sourcetext: 'Find icon vector created by rawpixel.com - www.freepik.com',
+    source: 'https://www.freepik.com/vectors/find-icon',
   },
 
   {
     id: 5,
-    image: 'https://activeloc.com/wp-content/uploads/2021/01/istockphoto-1070519266-612x612-1.jpg',
+    image: 'https://img.freepik.com/free-vector/colleagues-working-together-project_74855-6308.jpg',
     title: 'Join Projects!',
     body: 'Found a project you like? Let the project owner know by requesting to join. Don\'t be shy!',
+    sourcetext: 'Project team vector created by pch.vector - www.freepik.com',
+    source: 'https://www.freepik.com/vectors/project-team',
   },
 
   {
     id: 6,
-    image: 'https://png.pngtree.com/element_our/20190602/ourlarge/pngtree-team-staff-and-light-bulb-illustration-image_1385479.jpg',
+    image: 'https://img.freepik.com/free-vector/bulb-with-business-icons-flat-design_1212-121.jpg',
     title: 'Stay up to date!',
     body: 'Keep up to date with the latest activity on your projects with the notification system. Easily manage you\'re invitations and join requests.',
+    sourcetext: 'Company team vector created by photoroyalty - www.freepik.com',
+    source: 'https://www.freepik.com/vectors/company-team',
   },
 ];
 
@@ -93,6 +105,13 @@ const Home = () => {
                   image={card.image}
                   alt="displayimg"
                 />
+                <Typography variant="subtitle2" align="center" color="text.secondary" style={{ fontSize: 10 }}>
+                  <a href={card.source} style={{ textDecoration: 'none', color: 'inherit' }}>
+                    {' '}
+                    {card.sourcetext}
+                    {' '}
+                  </a>
+                </Typography>
                 <CardContent sx={{ flexGrow: 1 }}>
                   <Typography gutterBottom variant="h5" component="h2">
                     {card.title}
